1b4f2123de13d54ef0653ce3ee1b8c4bebeed35b
MarceAriel99 <mrondan@fi.uba.ar>
2023-07-10 16:13:21 -0300

    Comentarios, pequeña optimización y renombro el script

--- script.R
+++ script.R
@@ -3,25 +3,27 @@ set.seed(1)
 
 ############ Funciones utiles ############
 
+# Nivel de riesgo
+a <- 0.05
+
 # Devuelve el extremo inferior y superior para el pivote T1
 T1_IC <- function(sample, n) {
   mean <- mean(sample)
-  sup <- (1 / (-(qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
-  inf <- (1 / ((qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
+  sup <- (1 / (-(qnorm(1-(a/2))*mean / sqrt(n)) + mean))
+  inf <- (1 / ((qnorm(1-(a/2))*mean / sqrt(n)) + mean))
   return(c(inf, sup))
 }
 # Devuelve el extremo inferior y superior para el pivote T2
 T2_IC <- function(sample, n) {
   sum <- sum(sample)
-  sup <- (qchisq(1 - (0.05 / 2), 2 * n) / (2 * sum))
-  inf <- (qchisq((0.05 / 2), 2 * n) / (2 * sum))
+  sup <- (qchisq(1 - (a/2),2*n) / (2*sum))
+  inf <- (qchisq((a/2),2*n) / (2*sum))
   return(c(inf, sup))
 }
 
 ############ Comienzo del Script ############
 
-# Defino las variables
-a <- 0.05
+# Defino las variables para los cálculos
 k <- 5000
 sizes <- c(10, 30, 100, 1000)
 lambdas <- c(1, 5)
@@ -49,14 +51,16 @@ for (lambda in lambdas) {
       # Genero una muestra de valores con distribuciones exponenciales con el parámetro dado
       sample <- rexp(n, lambda)
       
-      # Calculo el intervalo de confianza
-      t1_ic_inf <- T1_IC(sample, n)[1]
-      t1_ic_sup <- T1_IC(sample, n)[2]
+      # Calculo el intervalo de confianza para cada método
+      t1_ic_result <- T1_IC(sample, n)
+      t1_ic_inf <- t1_ic_result[1]
+      t1_ic_sup <- t1_ic_result[2]
       
-      t2_ic_inf <- T2_IC(sample, n)[1]
-      t2_ic_sup <- T2_IC(sample, n)[2]
+      t2_ic_result <- T2_IC(sample, n)
+      t2_ic_inf <- t2_ic_result[1]
+      t2_ic_sup <- t2_ic_result[2]
       
-      # Para esta iteración, guardo la longitud del IC y la cobertura.
+      # Para esta replicación, guardo la longitud del IC y la cobertura para cada método
       t1_long_ic <- append(t1_long_ic, t1_ic_sup - t1_ic_inf)
       t1_cobertura <- append(t1_cobertura, t1_ic_inf < lambda && lambda < t1_ic_sup)
       
@@ -65,8 +69,8 @@ for (lambda in lambdas) {
     }
     
     # Guardo los resultados en el data frame
-    t1_results <- rbind(t1_results, list(lambda=lambda, n=n, ic=mean(t1_long_ic), coverage=mean(t1_cobertura)))
-    t2_results <- rbind(t2_results, list(lambda=lambda, n=n, ic=mean(t2_long_ic), coverage=mean(t2_cobertura)))
+    t1_results <- rbind(t1_results, list(lambda=lambda, n=n, long_ic=mean(t1_long_ic), coverage=mean(t1_cobertura)))
+    t2_results <- rbind(t2_results, list(lambda=lambda, n=n, long_ic=mean(t2_long_ic), coverage=mean(t2_cobertura)))
   }
 }
 

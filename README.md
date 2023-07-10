fb5d29d216242207812103509473bc443137d0c1
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2023-07-10 05:53:01 -0300

    Code refactor & prettify

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
+
 # FIUBA - 1C2023 - Probabilidad y Estadística B - TP1
 
 Dejo acá los resultados para cada pivote, para futura referencia y revisión de errores.
 
-> results_t1
-  lambda    n         ic coverage
-1      1   10  2.2371439   0.8976
-2      1   30  0.8505177   0.9328
-3      1  100  0.4113021   0.9460
-4      1 1000  0.1245341   0.9494
-5      5   10 11.1348461   0.9012
-6      5   30  4.2631198   0.9290
-7      5  100  2.0573605   0.9402
-8      5 1000  0.6229905   0.9500
+    > t1_results
+     lambda    n         ic coverage
+    1      1   10  2.2371439   0.8976
+    2      1   30  0.8505177   0.9328
+    3      1  100  0.4113021   0.9460
+    4      1 1000  0.1245341   0.9494
+    5      5   10 11.1348461   0.9012
+    6      5   30  4.2631198   0.9290
+    7      5  100  2.0573605   0.9402
+    8      5 1000  0.6229905   0.9500 
+    
+    > t2_results
+     lambda    n        ic coverage
+    1      1   10 1.3659188   0.9444
+    2      1   30 0.7394558   0.9500
+    3      1  100 0.3951558   0.9504
+    4      1 1000 0.1240448   0.9486
+    5      5   10 6.7985326   0.9528
+    6      5   30 3.7064355   0.9484
+    7      5  100 1.9765956   0.9490
+    8      5 1000 0.6205429   0.9506
 
-> results_t2
-  lambda    n        ic coverage
-1      1   10 1.3659188   0.9444
-2      1   30 0.7394558   0.9500
-3      1  100 0.3951558   0.9504
-4      1 1000 0.1240448   0.9486
-5      5   10 6.7985326   0.9528
-6      5   30 3.7064355   0.9484
-7      5  100 1.9765956   0.9490
-8      5 1000 0.6205429   0.9506
-
-Estos resultados se obtuvieron con la primera versión terminada del script.
+Estos resultados se obtuvieron con la primera versión terminada del script. Cada vez que se haga un cambio, hay que comparar que los resultados coincidan (a menos que se descubra un error).
--- punto_2.R
+++ punto_2.R
@@ -1,19 +1,34 @@
-# Establezco el seed para poder repetir los resultados
+############ Establezco el seed para poder repetir los resultados ############
 set.seed(1)
 
+############ Funciones utiles ############
+
+# Devuelve el extremo inferior y superior para el pivote T1
+T1_IC <- function(sample, n) {
+  mean <- mean(sample)
+  sup <- (1 / (-(qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
+  inf <- (1 / ((qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
+  return(c(inf, sup))
+}
+# Devuelve el extremo inferior y superior para el pivote T2
+T2_IC <- function(sample, n) {
+  sum <- sum(sample)
+  sup <- (qchisq(1 - (0.05 / 2), 2 * n) / (2 * sum))
+  inf <- (qchisq((0.05 / 2), 2 * n) / (2 * sum))
+  return(c(inf, sup))
+}
+
+############ Comienzo del Script ############
+
 # Defino las variables
-a<-0.05
-k<-5000
-sizes<-c(10, 30, 100, 1000)
-lambdas<-c(1, 5)
+a <- 0.05
+k <- 5000
+sizes <- c(10, 30, 100, 1000)
+lambdas <- c(1, 5)
 
-# Defino un data frame para guardar los resultados
-results_t1 <- data.frame()
-results_t2 <- data.frame()
-
-# Inicializo los vectores para guardar los intervalos de confianza
-ic_t1<-c()
-ic_t2<-c()
+# Defino data frames para guardar los resultados
+t1_results <- data.frame()
+t2_results <- data.frame()
 
 # Por cada lambda...
 for (lambda in lambdas) {
@@ -22,41 +37,38 @@ for (lambda in lambdas) {
   for (n in sizes) {
     
     # Variables para guardar resultados parciales
-    long_ic_t1 <- c()
-    long_ic_t2 <- c()
-    cobertura_t1 <- c()
-    cobertura_t2 <- c()
+    t1_long_ic <- c()
+    t1_cobertura <- c()
+    
+    t2_long_ic <- c()
+    t2_cobertura <- c()
     
     # Por cada replicación...
     for (m in 1:k) {
-    
+      
       # Genero una muestra de valores con distribuciones exponenciales con el parámetro dado
-      sample<-rexp(n,lambda)
+      sample <- rexp(n, lambda)
       
-      # Calculo el promedio de la muestra
-      mean<-mean(sample)
+      # Calculo el intervalo de confianza
+      t1_ic_inf <- T1_IC(sample, n)[1]
+      t1_ic_sup <- T1_IC(sample, n)[2]
       
-      # Calculo el intervalo de confianza para el pivote 1
-      ic_t1[1]<-(1 / ((qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
-      ic_t1[2]<-(1 / (-(qnorm(1-(0.05/2))*mean / sqrt(n)) + mean))
-      
-      # Calculo el intervalo de confianza para el pivote 2
-      ic_t2[1]<-(qchisq((0.05/2), 2*n) / (2 * sum(sample)))
-      ic_t2[2]<-(qchisq(1-(0.05/2), 2*n) / (2 * sum(sample)))
+      t2_ic_inf <- T2_IC(sample, n)[1]
+      t2_ic_sup <- T2_IC(sample, n)[2]
       
       # Para esta iteración, guardo la longitud del IC y la cobertura.
-      long_ic_t1 <- append(long_ic_t1, ic_t1[2] - ic_t1[1])
-      cobertura_t1 <- append(cobertura_t1, ic_t1[1] < lambda && lambda < ic_t1[2])
-      long_ic_t2 <- append(long_ic_t2, ic_t2[2] - ic_t2[1])
-      cobertura_t2 <- append(cobertura_t2, ic_t2[1] < lambda && lambda < ic_t2[2])
+      t1_long_ic <- append(t1_long_ic, t1_ic_sup - t1_ic_inf)
+      t1_cobertura <- append(t1_cobertura, t1_ic_inf < lambda && lambda < t1_ic_sup)
       
+      t2_long_ic <- append(t2_long_ic, t2_ic_sup - t2_ic_inf)
+      t2_cobertura <- append(t2_cobertura, t2_ic_inf < lambda && lambda < t2_ic_sup)
     }
     
     # Guardo los resultados en el data frame
-    results_t1 <- rbind(results_t1, list(lambda=lambda, n=n, ic=mean(long_ic_t1), coverage=mean(cobertura_t1)))
-    results_t2 <- rbind(results_t2, list(lambda=lambda, n=n, ic=mean(long_ic_t2), coverage=mean(cobertura_t2)))
+    t1_results <- rbind(t1_results, list(lambda=lambda, n=n, ic=mean(t1_long_ic), coverage=mean(t1_cobertura)))
+    t2_results <- rbind(t2_results, list(lambda=lambda, n=n, ic=mean(t2_long_ic), coverage=mean(t2_cobertura)))
   }
 }
 
-results_t1
-results_t2
+t1_results
+t2_results
